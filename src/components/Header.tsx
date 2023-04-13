b107be05097a03a89ecd05074f3f2ee84f661229
Dmitrii Rodionov <drodionov.work@gmail.com>
2023-04-13 23:19:18 +0300

    Add layout components Use tailwindcss to make UI responsive Use react icons

---
+++ src/components/Header.tsx
@@ -0,0 +1,30 @@
+import {useRouter} from "next/router";
+import React, {useCallback} from "react";
+import {BiArrowBack} from "react-icons/bi";
+
+interface HeaderProps {
+  label: string;
+  showBackError?: boolean;
+}
+
+const Header: React.FC<HeaderProps> = ({label, showBackError}) => {
+  const router = useRouter();
+
+  const handleBack = useCallback(() => {
+    router.back();
+  }, []);
+
+  return (
+      <div className="border-b=[1px] border-neutral-800 p-5">
+        <div className={"flex flex-row items-center gap-2"}>
+          {
+              showBackError && <BiArrowBack onClick={handleBack} color={"white"} size={20}
+                                            className={"cursor-pointer hover:opacity-70 transition"}/>
+          }
+          <h1 className={"text-white text-xl font-semibold"}>{label}</h1>
+        </div>
+      </div>
+  )
+}
+
+export default Header;
