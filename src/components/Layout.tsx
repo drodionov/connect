de39208ae34fafbd561bc721d251a532f1995ca6
Dmitrii Rodionov <drodionov.work@gmail.com>
2023-04-14 10:59:28 +0300

    Add Head element

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Sidebar from "@/components/layout/Sidebar";
 import FollowBar from "@/components/layout/FollowBar";
+import Head from "next/head";
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -8,17 +9,24 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({children}) => {
   return (
-      <div className="h-screen bg-black">
-        <div className="container h-full mx-auto xl:px-30 max-w-6xl">
-          <div className="grid grid-cols-4 h-full">
-            <Sidebar/>
-            <div className="col-span-3 lg:col-span-2 border-x-[1px] border-neutral-800">
-              {children}
+      <>
+        <Head>
+          <title>Connect App [Tweeter Clone]</title>
+          <meta name={'description'}
+                content={'This Is The Best Twitter Clone Ever %)'}/>
+        </Head>
+        <div className="h-screen bg-black">
+          <div className="container h-full mx-auto xl:px-30 max-w-6xl">
+            <div className="grid grid-cols-4 h-full">
+              <Sidebar/>
+              <div className="col-span-3 lg:col-span-2 border-x-[1px] border-neutral-800">
+                {children}
+              </div>
+              <FollowBar/>
             </div>
-            <FollowBar/>
           </div>
         </div>
-      </div>
+      </>
   )
 }
 
