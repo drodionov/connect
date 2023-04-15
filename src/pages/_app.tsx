7c1108f9e1039e3c3be08c0cc3b109c1a8d4879e
Dmitrii Rodionov <drodionov.work@gmail.com>
2023-04-16 00:39:27 +0300

    Add Register & Login Modal Open Login form on click Tweet Add register modal hook Link Register and Login forms

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,13 @@
 import '@/styles/globals.css'
 import type {AppProps} from 'next/app'
 import Layout from "@/components/Layout";
+import LoginModal from "@/components/modals/LoginModal";
+import RegisterModal from "@/components/modals/RegisterModal";
 
 const App = ({Component, pageProps}: AppProps) => (
     <>
+      <RegisterModal/>
+      <LoginModal/>
       <Layout>
         <Component {...pageProps} />
       </Layout>
