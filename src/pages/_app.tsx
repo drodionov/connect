83867dbe39a3036af8ed1712d48f6a4c6cd763e6
Dmitrii Rodionov <drodionov.work@gmail.com>
2023-04-15 23:48:20 +0300

    Add Modal component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,11 @@ import type {AppProps} from 'next/app'
 import Layout from "@/components/Layout";
 
 const App = ({Component, pageProps}: AppProps) => (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
 );
 
 export default App;
