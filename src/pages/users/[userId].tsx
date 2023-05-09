9f503aabffd6ed659f7978b072d7d39a0581e4ff
Dmitrii Rodionov <drodionov.work@gmail.com>
2023-05-09 20:21:48 +0300

    Add spinner lib Implement initial UserView

---
+++ src/pages/users/[userId].tsx
@@ -0,0 +1,28 @@
+import Header from "@/components/Header";
+import {useRouter} from "next/router";
+import useUser from "@/hooks/useUser";
+import {ClipLoader} from "react-spinners";
+
+const UserView = () => {
+  const router = useRouter();
+  const {userId} = router.query;
+
+  const {data: fetchedUser, isLoading} = useUser(userId as string);
+
+  if (isLoading || !fetchedUser) {
+    return (
+        <div className="flex justify-center items-center h-full">
+          <ClipLoader color="lightblue" size={80}/>
+        </div>
+    )
+  }
+
+  return (
+      <>
+        <Header showBackError label={fetchedUser?.name}/>
+
+      </>
+  )
+}
+
+export default UserView;
