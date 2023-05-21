d91c56d12fb7a37695607d1d458b33f92c29cced
Dmitrii Rodionov <drodionov.work@gmail.com>
2023-05-21 12:53:26 +0300

    Implement API for follow functionality

---
+++ src/pages/api/follow.ts
@@ -0,0 +1,57 @@
+import {NextApiRequest, NextApiResponse} from "next";
+import serverAuth from "@/libs/serverAuth";
+import prisma from "@/libs/prismadb";
+
+const handler = async (
+    req: NextApiRequest,
+    res: NextApiResponse
+) => {
+  if (req.method !== 'POST' && req.method !== 'DELETE') {
+    return res.status(405).end();
+  }
+
+  try {
+
+    const {userId} = req.body;
+    const {currentUser} = await serverAuth(req, res);
+
+    if (!userId || typeof userId !== 'string') {
+      throw new Error('Invalid ID');
+    }
+
+    const user = await prisma.user.findUnique({
+      where: {
+        id: userId
+      }
+    });
+
+    if (!user) {
+      throw new Error("Invalid ID");
+    }
+
+    let updateFollowingIds = [...(user.followingIds || [])];
+
+    if (req.method === 'POST') {
+      updateFollowingIds.push(userId);
+    }
+
+    if (req.method === 'DELETE') {
+      updateFollowingIds = updateFollowingIds.filter(followingId => followingId !== userId);
+    }
+
+    const updatedUser = await prisma.user.update({
+      where: {
+        id: currentUser.id
+      },
+      data: {
+        followingIds: updateFollowingIds
+      }
+    });
+
+    return res.status(200).json(updatedUser);
+  } catch (error) {
+    console.log("While trying to follow: " + error);
+    return res.status(503).end();
+  }
+};
+export default handler
